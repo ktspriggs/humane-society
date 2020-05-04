5d7e89b0a0bc78c542dd3ecf9a1857cc18ce73c6
ktspriggs <katiespriggs@Katies-MBP.fios-router.home>
2020-05-04 19:13:32 -0400

    Fixed button responsivness and navigation bar spacing and started creating search bar.

--- js/index.js
+++ js/index.js
@@ -3,14 +3,29 @@ $(document).ready(function (){
     var menuClosed = $('.menu-closed');
     var menuOpen = $('.menu-open');
     var mobileMenu = $('.mobile-menu');
-    var mobileMenuLink = $('.menu li a');
+    var mainContent = $('.main-content');
+    var searchImg = $('.search-img');
+    //var mobileMenuLink = $('.menu li a');//
 
-    //menu opens and closes//
+    //menu opens and closes and main content opacity changes//
     mobileMenuBurger.on('click', function () {
         menuClosed.toggleClass('display-none');
         menuOpen.toggleClass('display-none');
         mobileMenu.toggleClass('display-none');
+        mainContent.toggleClass('opacity');
     });
+
+    //click on menuClosed menu transitions open//
+    menuClosed.on('click', function () {
+        mobileMenu.slideToggle("slow");
+    });
+
+    //click on menuOpen menu transitions closed//
+    menuOpen.on('click', function () {
+        mobileMenu.slideToggle("slow");
+    });
+
+    //click on searchImg searchBar opens//
     
 
 
